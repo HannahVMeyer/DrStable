e3e9322fbfb46ce1467fdf8dd46475984ae3271b
HannahVMeyer <hannah.v.meyer@googlemail.com>
2018-07-05 16:15:09 +0100

    Add function for subset estimation

--- R/DimReduction.R
+++ R/DimReduction.R
@@ -1,5 +1,7 @@
 #' Compute dimensionality reduction
 #'
+#' Compute low-dimensional representation of dataset.
+#'
 #' @param Y [N x P] data matrix for which the dimensionality of P should be
 #' reduced
 #' @param method Dimensionality reduction method [string] to be applied; one of
@@ -18,8 +20,7 @@
 #' computation on multiple cpu cores is used with \code{\link{lle::calc_k}}.
 #' @param verbose [logical] If set, progress messages are printed to standard 
 #' out
-#' @return named list of results from dimensionality reduction; if multiple 
-#' methods were specified, each named list entry contains the following:
+#' @return named list of results from dimensionality reduction:
 #' Y_red:  named list with dimensionality reduced phenotypes (reducedY) and 
 #' object returned by specified dimensionality reduction method (results) with
 #' additional output
@@ -27,19 +28,30 @@
 #' dimensionality reduction
 #'
 #' @export
-computeDimReduction <-  function(method, Y, optN=NULL, ndim=NULL, 
+#' @examples 
+#' # Generate some data
+#' x <- matrix(rnorm(10000), nrow=10, ncol=100)
+#' y <- x %*% diag(nrow=100) * rnorm(100)
+#' dr <- computeDimReduction(y, method="MDS")
+computeDimReduction <-  function(Y, method, optN=NULL, ndim=NULL, 
                                  kmin=1, kmax=40, verbose=FALSE, 
                                  parallel=FALSE) {
     # phenotype dimensions
     N <- nrow(Y)
     P <- ncol(Y)
     
+    if (ndim > P) {
+        stop("ndim has to less than or equal to original column dimension")
+    }
     # number of dimensions to estimate
     if (is.null(ndim)) {
         if (P < N) {
             ndim <- P
         } else {
             ndim <- N
+        }
+        if (method == "MDS") {
+            ndim <- ndim -1
         }
     }
     if (is.null(optN) && any(method %in% c("LLE", "LaplacianEigenmaps", 
@@ -51,26 +63,19 @@ computeDimReduction <-  function(method, Y, optN=NULL, ndim=NULL,
                                   parallel=parallel, plotres=FALSE)
         optN <- neighbours$k[which.min(neighbours$rho)]
     }
-    tmp <- lapply(method, function(m, Y, optN, ndim) {
-        vmessage(c("Running dimensionality reduction:", m), verbose=verbose)
-        
-        # dimensionalityReduction
-        red <- methodsDimReduction(Y=Y, method=m, ndim=ndim, optN=optN)
-        rownames(red$reducedY) <- rownames(Y)
-        colnames(red$reducedY) <- paste("DR", 1:ncol(red$reducedY), sep="")
-        
-        # estimate trustworthiness and continuity
-        neighbours <- round(1:5 * nrow(Y)/100)
-        M <- TandC(original=Y, reduced=red$reducedY, 
-                   neighbours=neighbours)
-        return(list(Y_red=red, M=M))
-    }, Y=Y, optN=optN, ndim=ndim)
-    if (length(tmp) == 1) {
-        tmp <- tmp[[1]]
-    } else {
-        names(tmp) <- method
-    }
-    return(tmp)
+
+    vmessage(c("Running dimensionality reduction:", method), verbose=verbose)
+    
+    # dimensionalityReduction
+    red <- methodsDimReduction(Y=Y, method=method, ndim=ndim, optN=optN)
+    rownames(red$reducedY) <- rownames(Y)
+    colnames(red$reducedY) <- paste("DR", 1:ncol(red$reducedY), sep="")
+    
+    # estimate trustworthiness and continuity
+    neighbours <- round(1:5 * nrow(Y)/100)
+    M <- TandC(original=Y, reduced=red$reducedY, 
+               neighbours=neighbours)
+    return(list(Yred=red$reducedY, M=M, additionalResults=red$results))
 }
 
 #' Wrapper function for dimensionality reduction methods
@@ -149,5 +154,54 @@ methodsDimReduction <- function(Y, ndim,
     return(list(reducedY=reducedY, results=results))
 }
 
-
-
+#' Compute dimensionality reduction for subsets of the input data
+#' 
+#'
+#' @param Y [N x P] data matrix for which the dimensionality of P should be
+#' reduced,
+#' @param seed [int] seed to initialise random number generator for drawing
+#' subsets of Y.
+#' @param size [float] proportion of samples from total number of samples to
+#' to choose for each subset.
+#' @param nrSubsets [int] number of subsets to generate and apply dimensionality
+#' reduction to.
+#' @param method dimensionality reduction method [string] to be applied; one of
+#' DiffusionsMaps, DRR, ICA, LLE, Isomap, LaplacianEigenmap, MDS, PCA, kPCA, 
+#' nMDS, tSNE and PEER. 
+#' @param optN optimal number [int] of neighbours to consider for dimensionality
+#' reduction; relevant for methods LLE, LaplacianEigenmaps, Isomap and tSNE. If 
+#' not provided, will be estimated via \code{\link{lle::calc_k}}.
+#' @param ndim maximum dimensionality [int] to retain in the data; large values
+#' can cause long computation times; if not provided max(P,N) is chosen.
+#' @param kmin if optN is not provided, kmin [int] specifies the minimum number
+#' of neighbours supplied to  \code{\link{lle::calc_k}}.
+#' @param kmax if optN is not provided, kmax [int] specifies the maximum number
+#' of neighbours supplied to  \code{\link{lle::calc_k}}.
+#' @param parallel if optN is not provided and parallel TRUE, parallel 
+#' computation on multiple cpu cores is used with \code{\link{lle::calc_k}}.
+#' @param verbose [logical] If set, progress messages are printed to standard 
+#' out.
+#' @return list of size nrSubsets, containing at each entry a named list of 
+#' results from \link{\code{computeDimReduction}}:
+#' Y_red:  named list with dimensionality reduced phenotypes (reducedY) and 
+#' object returned by specified dimensionality reduction method (results) with
+#' additional output
+#' M: vector [double] with Trustworthiness and Continuity estimates for the
+#' dimensionality reduction
+#'
+#' @export
+subsetDimReduction <- function(Y, seed, method, size=0.8, nrSubsets=10, 
+                               optN=NULL, ndim=NULL, kmin=1, kmax=40, 
+                               verbose=FALSE, parallel=FALSE) {
+    set.seed(seed)
+    sample_matrix <- sapply(1:nrSubsets, function(x) sample(nrow(Y), size*nrow(Y)))
+    dr <- lapply(1:ncol(sample_matrix), function(x){
+        y_cv <- Y[sample_matrix[,x],]
+        vmessage(c("Crossvalidation:", x), verbose=verbose)
+        dimRed <- computeDimReduction(Y=y_cv, ndim=ndim, method=method, 
+                                  kmin=kmin, kmax=kmax, optN=optN,
+                                  verbose=verbose, parallel=parallel)
+        return(dimRed)
+    })
+    return(dr)
+}
