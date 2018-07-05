8f31cff6e15b2ec4614a34deef08ffa6bc4cf641
HannahVMeyer <hannah.v.meyer@googlemail.com>
2018-07-05 16:17:17 +0100

    Save plots separately

--- R/plotting.R
+++ R/plotting.R
@@ -1,31 +1,36 @@
-plotStability <- function(corpass, maxcor, medians_pass, color) {
-    p_pass <- ggplot(corpass, aes(x=as.factor(threshold), y=value))
+plotStability <- function(stability, colorPalette="YlGn", saveTo=NULL) {
+    p_pass <- ggplot(stability$pass_long, 
+                     aes(x=as.factor(threshold), y=componentsPass))
     p_pass <- p_pass +
         geom_boxplot(outlier.colour=NA) + 
         geom_jitter(width = 0.2, size=0.5) +
         labs(x = "Correlation threshold for filtering", 
              y = "Number of components") +
-        geom_text(data = medians_pass, aes(x = as.factor(threshold), 
-                                           y = value, label= value), 
+        geom_text(data = stability$medians_pass, aes(x = as.factor(threshold), 
+                                           y = componentsPass, 
+                                           label= componentsPass), 
                   nudge_x =-0.7, color="red") +
         theme_bw()
     
-    p_maxcor <- ggplot(maxcor, aes(x=as.factor(component), y=abs(value), 
+    p_maxcor <- ggplot(stability$maxcor_long, 
+                       aes(x=as.factor(component), y=abs(correlation), 
                                         fill=as.factor(threshold)))
     p_maxcor <- p_maxcor + 
         geom_boxplot(outlier.colour=NA) +
-        scale_fill_manual(
-            values=color[(16-length(levels(as.factor(maxcor$threshold)))):16]) +
+        scale_fill_brewer(type="seq", palette=colorPalette) +
         guides(fill=guide_legend(title="Threshold")) +
         labs(x = "Components", y = "Spearman correlation") +
         theme_bw() +
         theme(axis.text.x = element_text(angle=90))
     
-    ggsave(filename=paste(outdir, "CorrelationComponentsPass_", method, 
-                          ".pdf", sep=""), 
+    if (!is.null(saveTo)) {
+        ggsave(filename=paste(saveTo, "/CorrelationComponentsPassThreshold_", 
+                              method, ".pdf", sep=""), 
            plot=p_pass, height=12, width=16)
-    ggsave(filename=paste(outdir, "CorrelationComponentsMaxcor_", method, 
-                          ".pdf", sep=""), 
+        ggsave(filename=paste(saveTo, "/CorrelationThresholdPerComponent_", 
+                              method, ".pdf", sep=""), 
            plot=p_maxcor, height=12, width=16)
+    }
+    return(list(pass=p_pass, corr=p_maxcor))
 }
     
