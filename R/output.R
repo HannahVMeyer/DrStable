54992a3d3583ccb6fe314260db30f0b045ca88a8
Hannah <hannah.v.meyer@gmail.com>
2018-09-04 20:54:03 +0000

    document output function; save as csv

--- R/output.R
+++ R/output.R
@@ -1,29 +1,45 @@
 #' Save results of dimensionality reduction
-#' 
-#' @param results named list of results from dimensionality reduction; if 
-#' multiple methods were specified, each named list entry containing:
-#' Y_red: named list with dimensionality reduced phenotypes (reducedY) and 
+#'
+#' @param results named list of results from dimensionality reduction;
+#' Y_red: named list with dimensionality reduced phenotypes (reducedY) and
 #' object returned by specified dimensionality reduction method (results) with
 #' additional output.
 #' M: vector [double] with Trustworthiness and Continuity estimates for the
 #' dimensionality reduction.
-#' @param outdir path/to/directory [string] where results will be saved.
-saveDimReduction <- function(results, outdir) {
-    saveRDS(results$Y_red$results, paste(outdir, "/", m, "_results.rds", 
+#' @param name [string] name of output file
+#' @param outdir [string] full path/to/directory [string] where results will be
+#' saved. Full file name will be created by outdir/name_method.csv.
+#' Alternatively, prefix can be provided to create file prefix_method.csv.
+#' @param prefix [string] of output file name, containing full
+#' path/to/directory [string] where results will be saved. Results will be saved
+#' as prefix_method.csv
+#' @param  method [string] name of dimensionality reduction method; will be
+#' appended to output file name.
+#' @export
+saveDimReduction <- function(results, method, outdir=NULL, name=NULL,
+    prefix=NULL){
+    if ((is.null(name) || is.null(outdir)) && is.null(prefix)) {
+        stop("Either name and outdir, or prefix have to be specified for saving
+             dimensionality reduction results")
+    }
+    if (is.null(prefix)) {
+        prefix <- paste(outdir, "/", name, "_", sep="")
+    }
+    saveRDS(results, paste(prefix, method, "_results.rds",
                                          sep=""))
-    write.table(resutls$Y_red$reducedY, paste(outdir, ",",  m, "_reducedY.csv", 
-                                    sep=""), 
+    write.table(results$Yred, paste(prefix,  method,
+                                              "_reducedY.csv", sep=""),
                 quote=FALSE, row.names=TRUE, col.names=NA, sep=",")
-    write.table(results$M, paste(outdir, ",",  m, "_TandC.csv", 
-                         sep=""), 
+    write.table(results$M, paste(prefix,  method, "_TandC.csv",
+                         sep=""),
                 quote=FALSE, row.names=TRUE, col.names=NA, sep=",")
 }
 
-#' Save results from analyseDimreduction 
+#' Save results from analyseDimreduction
 write_medians <- function(threshold) {
     sapply(threshold, function(thr) {
      write.table(paste(medians_maxcor$component[medians_maxcor$threshold >= 
-                                                   thr], 
+                                                   thr],
                       collapse = ","),  
                 paste(output ,"/ComponentsPassingThr", thr, "_", method, 
                       ".csv", sep=""), 
