70070f562e8a289b7dba21f68ce23871831649b6
HannahVMeyer <hannah.v.meyer@googlemail.com>
2019-07-30 21:33:23 -0400

    Rename, update dependencies

---
+++ R/drStable.R
@@ -0,0 +1,12 @@
+#' drStable
+#'
+#' @docType package
+#' @name drStable
+#' @import ggplot2 
+#' @importFrom utils read.table read.csv write.table type.convert
+#' @importFrom optparse make_option parse_args OptionParser
+#' @importFrom dplyr filter
+#' @importFrom MCMCpack procrustes
+#' @importFrom reshape2 melt
+#' @importFrom stats dist lm median na.omit qnorm
+NULL
